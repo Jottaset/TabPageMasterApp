cf0a795a98a48cd9de9ad35921cb0e996c68b70c
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2019-03-12 21:26:21 -0400

    first commit

---
+++ TabPageMasterApp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TabPageMasterApp
+{
+    public partial class MainPage : ContentPage
+    {
+        void Concatenando(object sender, System.EventArgs e)
+        {
+            label_result.Text = label_01.Text + " " + label_02.Text;
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
